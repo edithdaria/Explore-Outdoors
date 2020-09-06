17bb8d7dad99b2b4d53bfef369565f0d23eb799b
Tiger <63274912+edithdaria@users.noreply.github.com>
2020-09-06 13:02:54 -0400

    mongojs connection established

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 //db
-const db = mongojs("parkdb", ["parks"]);
+//const db = mongojs("parkdb", ["parks"]);
+const db = mongojs("process.env.MONGODB_URI", ["parks"]);
 
 //port setup
 const PORT = process.env.PORT || 3000;
@@ -32,10 +33,8 @@ app.use(express.static("./"));
 
 
 //mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://user:Password1@clusterexplore.uqtfh.mongodb.net/parkdb?retryWrites=true&w=majority", { useNewUrlParser: true });
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://user:Password1@clusterexplore.uqtfh.mongodb.net/parkdb?retryWrites=true&w=majority", { useNewUrlParser: true});
-mongoose.connection.on('connected', () => console.log('Connected'));
-mongoose.connection.on('error', () => console.log('Connection failed with - ',err));
+//mongoose.connection.on('connected', () => console.log('Connected'));
+//mongoose.connection.on('error', () => console.log('Connection failed with - ',err));
 
 //require('./routes/api-routes')(app);
 
